e341929af12d069761380669d04b78cb8222a81e
chick <chick@qrhino.com>
2016-12-14 14:37:12 -0800

    Invert reset This is initial machinery to invert reset because some of the Craft circuits require reset on low

---
+++ src/test/scala/dsptools/ResetInverterSpec.scala
@@ -0,0 +1,11 @@
+// See LICENSE for license details.
+
+package dsptools
+
+import org.scalatest.{FreeSpec, Matchers}
+
+class ResetInverterSpec extends FreeSpec with Matchers {
+  "Reset inverter should invert all usages of reset, including register inits in a circuit" - {
+
+  }
+}
