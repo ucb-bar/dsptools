fdfdb355fe4b83b65c8297e963a1235a66a645a0
chick <chick@qrhino.com>
2017-01-17 11:34:17 -0800

    Fixed bad trailing space in DspRealFactory#create instance, meant black boxes were not being found Created a smaller test case for LnSpec to figure the above out

---
+++ src/test/scala/dsptools/numbers/LnSpec.scala
@@ -0,0 +1,40 @@
+// See LICENSE for license details.
+
+package dsptools.numbers
+
+import chisel3._
+import chisel3.util._
+import chisel3.testers.BasicTester
+import chisel3.iotesters.{ChiselFlatSpec, PeekPokeTester, TesterOptionsManager}
+import dsptools.{DspTester, ReplOptionsManager}
+import org.scalatest.FreeSpec
+
+class LnModule extends Module {
+  val io = IO(new Bundle {
+    val num = Input(DspReal(1.0))
+    val ln = Output(DspReal(1.0))
+  })
+
+  io.ln := io.num.ln()
+}
+
+class LnTester(c: LnModule) extends DspTester(c) {
+  dspPoke(c.io.num,11.0)
+  private val x = dspPeek(c.io.ln)
+  println(s"poked 1.0 got $x expected ${math.log(11.0)}")
+
+}
+class LnSpec extends FreeSpec {
+  "ln should work" in {
+    dsptools.Driver.execute(() => new LnModule) { c =>
+      new LnTester(c)
+    }
+  }
+}
+
+object LnTester extends App {
+  val manager = new ReplOptionsManager
+  if(manager.parse(args)) {
+      dsptools.Driver.executeFirrtlRepl(() => new LnModule, manager)
+  }
+}
