4798601b7b19e876daad28ca69d155d493b428f7
Chick Markley <chick@qrhino.com>
2017-01-23 15:57:31 -0800

    Invert reset (#24)
    * Invert reset
    This is initial machinery to invert reset because some of the Craft circuits require reset on low
    
    * Invert reset
    This is initial machinery to invert reset because some of the Craft circuits require reset on low
    As implemented this requires a top level module annotation to be created.  This may not be
    the desired pattern
    
    * Change Test Module to something other than Unit

---
+++ src/test/scala/dsptools/ResetInverterSpec.scala
@@ -0,0 +1,43 @@
+// See LICENSE for license details.
+
+package dsptools
+
+import chisel3._
+import chisel3.util.RegInit
+import dsptools.firrtltools.ResetInverter
+import firrtl._
+import org.scalatest.{FreeSpec, Matchers}
+
+class ExampleModuleNeedsResetInverted extends Module with ResetInverter {
+  val io = IO(new Bundle {
+    val out = Output(UInt(32.W))
+  })
+
+  val r = RegInit(0.U)
+
+  invert(this)
+}
+
+class ResetNSpec extends FreeSpec with Matchers {
+
+  "Inverting reset needs to be done throughout module" in {
+    val optionsManager = new ExecutionOptionsManager("dsptools") with HasChiselExecutionOptions with HasFirrtlOptions {
+      firrtlOptions = firrtlOptions.copy(compilerName = "low")
+    }
+    chisel3.Driver.execute(optionsManager, () => new ExampleModuleNeedsResetInverted) match {
+      case ChiselExecutionSucccess(_, chirrtl, Some(FirrtlExecutionSuccess(_, firrtl))) =>
+        chirrtl should include ("input reset :")
+        chirrtl should not include "input reset_n :"
+        chirrtl should not include "node reset = not(reset_n)"
+
+        firrtl should include ("input reset_n :")
+        firrtl should include ("node reset = not(reset_n)")
+        firrtl should not include "input reset :"
+
+        // println(s"Chirrtl:\n$chirrtl") //scalastyle:off regex
+        // println(s"Firrtl:\n$firrtl") //scalastyle:off regex
+      case _ =>
+        // bad
+    }
+  }
+}
