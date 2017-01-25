ab4b20d7f4f3e436c68243017ec4e0a2738e00d1
grebe <rigge@berkeley.edu>
2017-01-25 14:48:58 -0800

    Add travis (#7)

---
+++ .install_deps.sh
@@ -0,0 +1,35 @@
+set -e
+set -x
+# Install Verilator (http://www.veripool.org/projects/verilator/wiki/Installing)
+if [ ! -d "$INSTALL_DIR" ]; then
+    mkdir -p "$INSTALL_DIR"
+fi
+
+if [ ! -d $TRAVIS_BUILD_DIR/lib ]; then
+    mkdir -p $TRAVIS_BUILD_DIR/lib
+fi
+
+orgs=( ucb-bar ucb-bar ucb-bar ucb-bar ucb-bar ucb-art grebe )
+repos=( firrtl chisel3 firrtl-interpreter chisel-testers dsptools rocket-chip testchipip )
+branches=( master master master master master craftFork chisel3switch )
+
+for idx in "${!repos[@]}"; do
+    org=${orgs[$idx]}
+    repo=${repos[$idx]}
+    branch=${branches[$idx]}
+    if [ ! -d $INSTALL_DIR/$repo ]; then
+        git clone --recursive --depth=2 "https://github.com/$org/$repo.git" "$INSTALL_DIR/$repo" -b $branch
+    fi
+    cd $INSTALL_DIR/$repo
+    if [ ! -e lib ]; then
+        ln -s $TRAVIS_BUILD_DIR/lib lib
+    fi
+    git fetch --all
+    git checkout $branch
+    git pull
+    sbt publish-local
+    if [ "$repo" == "rocket-chip" ]; then
+        sbt pack
+        cp target/pack/lib/*.jar $TRAVIS_BUILD_DIR/lib
+    fi
+done
