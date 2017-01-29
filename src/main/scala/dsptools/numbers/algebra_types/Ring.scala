b2ca3683feb2d0458b6ed4bc4d7ce4296e80bb3f
Angie <shunshou@gmail.com>
2017-01-29 02:37:10 -0800

    minor changes to ring/spire

--- src/main/scala/dsptools/numbers/algebra_types/Ring.scala
+++ src/main/scala/dsptools/numbers/algebra_types/Ring.scala
@@ -1,5 +1,7 @@
 package dsptools.numbers
 
+import chisel3.Data
+
 /* Needs to be redefined from spire */
 object Ring {
   def apply[A <: Data](implicit A: Ring[A]): Ring[A] = A
--- src/main/scala/dsptools/numbers/algebra_types/Spire.scala
+++ src/main/scala/dsptools/numbers/algebra_types/Spire.scala
@@ -12,4 +12,3 @@ package object numbers {
 
   val Multiplicative = spire.algebra.Multiplicative
 }
-
