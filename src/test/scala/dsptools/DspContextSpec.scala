6e73dfde75acdff6757dfab93a4bea6c1acb74ba
Angie <shunshou@gmail.com>
2017-02-06 23:50:31 -0800

    refactored dspcontext; moved with #'s

--- src/test/scala/dsptools/DspContextSpec.scala
+++ src/test/scala/dsptools/DspContextSpec.scala
@@ -9,34 +9,34 @@ import org.scalatest.{Matchers, FreeSpec}
 class DspContextSpec extends FreeSpec with Matchers {
   "Context handling should be unobtrusive and convenient" - {
     "There should be a default available at all times" in {
-      DspContext.current.binaryPoint should be (Some(DspContext.DefaultBinaryPoint))
+      DspContext.current.binaryPoint should be (Some(DspContext.defaultBinaryPoint))
     }
 
     "it can be very to override for simple alterations" in {
-      DspContext.current.binaryPoint should be (Some(DspContext.DefaultBinaryPoint))
+      DspContext.current.binaryPoint should be (Some(DspContext.defaultBinaryPoint))
 
       DspContext.withBinaryPoint(-22) {
         DspContext.current.binaryPoint.get should be (-22)
       }
 
-      DspContext.current.binaryPoint should be (Some(DspContext.DefaultBinaryPoint))
+      DspContext.current.binaryPoint should be (Some(DspContext.defaultBinaryPoint))
     }
 
     "it should be easy to override when using multiples" in {
-      DspContext.current.binaryPoint should be (Some(DspContext.DefaultBinaryPoint))
-      DspContext.current.overflowType should be (DspContext.DefaultOverflowType)
+      DspContext.current.binaryPoint should be (Some(DspContext.defaultBinaryPoint))
+      DspContext.current.overflowType should be (DspContext.defaultOverflowType)
 
       DspContext.alter(DspContext.current.copy(binaryPoint = Some(77), overflowType = Saturate)) {
         DspContext.current.binaryPoint.get should be (77)
         DspContext.current.overflowType should be (Saturate)
       }
 
-      DspContext.current.binaryPoint should be (Some(DspContext.DefaultBinaryPoint))
-      DspContext.current.overflowType should be (DspContext.DefaultOverflowType)
+      DspContext.current.binaryPoint should be (Some(DspContext.defaultBinaryPoint))
+      DspContext.current.overflowType should be (DspContext.defaultOverflowType)
     }
 
     "it should work multi-threaded and return values of block" in {
-      DspContext.current.numBits should be (Some(DspContext.DefaultNumBits))
+      DspContext.current.numBits should be (Some(DspContext.defaultNumBits))
 
       val points = (0 to 100).toParArray.map { n =>
         DspContext.withNumBits(n) {
@@ -46,7 +46,7 @@ class DspContextSpec extends FreeSpec with Matchers {
       }
       points.zipWithIndex.foreach { case (p: Int, i: Int) => p should be (i * i)}
 
-      DspContext.current.numBits should be (Some(DspContext.DefaultNumBits))
+      DspContext.current.numBits should be (Some(DspContext.defaultNumBits))
     }
   }
 }
