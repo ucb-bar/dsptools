b807e26a0c3faecf977c78ed85091d70f974468e
Stevo Bailey <stevo.bailey@eecs.berkeley.edu>
2017-03-23 17:15:26 -0700

    [stevo]: one more

--- src/test/scala/dsptools/numbers/NumbersSpec.scala
+++ src/test/scala/dsptools/numbers/NumbersSpec.scala
@@ -3,7 +3,7 @@
 package dsptools.numbers
 
 import chisel3._
-import chisel3.util.RegNext
+import chisel3.util._
 import dsptools._
 import chisel3.experimental.FixedPoint
 import dsptools.numbers.implicits._
