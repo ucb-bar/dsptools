6f63a1384a98fbd5aa4290c0e3534adf149bf046
Paul Rigge <rigge@berkeley.edu>
2017-09-06 15:13:29 -0700

    Shouldn't need to install Scala deps
    They should be on maven

--- .install_deps.sh
+++ .install_deps.sh
@@ -4,21 +4,3 @@ set -x
 if [ ! -d "$INSTALL_DIR" ]; then
     mkdir -p "$INSTALL_DIR"
 fi
-
-orgs=( ucb-bar ucb-bar ucb-bar ucb-bar )
-repos=( firrtl chisel3 firrtl-interpreter chisel-testers )
-branches=( master master master master )
-
-for idx in "${!repos[@]}"; do
-    org=${orgs[$idx]}
-    repo=${repos[$idx]}
-    branch=${branches[$idx]}
-    if [ ! -d $INSTALL_DIR/$repo ]; then
-        git clone --recursive --depth=2 "https://github.com/$org/$repo.git" "$INSTALL_DIR/$repo" -b $branch
-    fi
-    cd $INSTALL_DIR/$repo
-    git fetch --all
-    git checkout $branch
-    git pull
-    sbt publish-local
-done
