d7c284b8c60b3640d0d5bdbb842fcc5962eefe36
Angie <shunshou@gmail.com>
2017-10-30 15:33:07 -0700

    pipelining works -- hardcoded

--- src/test/scala/toys/MatMul.scala
+++ src/test/scala/toys/MatMul.scala
@@ -1,6 +1,7 @@
 package dsptools.toys
 
 import chisel3._
+import chisel3.util.ShiftRegister
 import dsptools.numbers._
 import breeze.linalg.{DenseVector, DenseMatrix}
 import generatortools.io.CustomBundle
@@ -15,6 +16,7 @@ import dsptools.intervals.tests._
 import org.scalatest.{Matchers, FlatSpec}
 
 // TODO: Ops need to work on 1x1 too!
+// TODO: Make # of pipeline stages not fixed!
 object Matrix {
   def tpe[T <: Data:RealBits](el: CustomBundle[CustomBundle[T]], depth: Int): Matrix[T] =
     new Matrix(el.cloneType, depth)
@@ -93,7 +95,7 @@ object Matrix {
 
 /** Recursive operations https://arxiv.org/pdf/1410.1599.pdf */
 class Matrix[T <: Data:RealBits](val elB: CustomBundle[CustomBundle[T]], val depth: Int) extends Bundle {
-  def el = Matrix.toSeq2D(elB)
+  val el = Matrix.toSeq2D(elB)
   el foreach { row => require(el.length == row.length, "Matrix must be square!") }
   val n = el.length
   require((n & (n - 1)) == 0, "n must be power of 2!")
@@ -101,16 +103,21 @@ class Matrix[T <: Data:RealBits](val elB: CustomBundle[CustomBundle[T]], val dep
     Matrix.tpe(elB, depth).asInstanceOf[this.type]
   }
   def isUnit = el.flatten.length == 1
-  def split(): Seq[Matrix[T]] = {
-    val el11 = Matrix(el.dropRight(n / 2).map(_.dropRight(n / 2)), depth)
-    el11.suggestName(s"split11_depth${el11.depth}")
-    val el12 = Matrix(el.dropRight(n / 2).map(_.drop(n / 2)), depth)
-    el12.suggestName(s"split12_depth${el12.depth}")
-    val el21 = Matrix(el.drop(n / 2).map(_.dropRight(n / 2)), depth)
-    el21.suggestName(s"split21_depth${el21.depth}")
-    val el22 = Matrix(el.drop(n / 2).map(_.drop(n / 2)), depth)
-    el22.suggestName(s"split22_depth${el22.depth}")
-    Seq(el11, el12, el21, el22)
+  def split: Seq[Matrix[T]] = {
+    if (el.flatten.length == 1) {
+      Seq(this)
+    }
+    else {
+      val el11 = Matrix(el.dropRight(n / 2).map(_.dropRight(n / 2)), depth)
+      el11.suggestName(s"split11_depth${el11.depth}")
+      val el12 = Matrix(el.dropRight(n / 2).map(_.drop(n / 2)), depth)
+      el12.suggestName(s"split12_depth${el12.depth}")
+      val el21 = Matrix(el.drop(n / 2).map(_.dropRight(n / 2)), depth)
+      el21.suggestName(s"split21_depth${el21.depth}")
+      val el22 = Matrix(el.drop(n / 2).map(_.drop(n / 2)), depth)
+      el22.suggestName(s"split22_depth${el22.depth}")
+      Seq(el11, el12, el21, el22)
+    }
   }
 
   def putTogether(xx11: Matrix[T], xx12: Matrix[T], xx21: Matrix[T], xx22: Matrix[T], op: String): Matrix[T] = {
@@ -126,8 +133,8 @@ class Matrix[T <: Data:RealBits](val elB: CustomBundle[CustomBundle[T]], val dep
     result
   }
   def + (b: Matrix[T]): Matrix[T] = {
-    val as = this.split()
-    val bs = b.split()
+    val as = this.split
+    val bs = b.split
     val aIs2x2 = as.map(_.isUnit).reduce(_ & _)
     val bIs2x2 = bs.map(_.isUnit).reduce(_ & _)
     if (aIs2x2 && bIs2x2) {
@@ -147,8 +154,8 @@ class Matrix[T <: Data:RealBits](val elB: CustomBundle[CustomBundle[T]], val dep
     }
   }
   def - (b: Matrix[T]): Matrix[T] = {
-    val as = this.split()
-    val bs = b.split()
+    val as = this.split
+    val bs = b.split
     val aIs2x2 = as.map(_.isUnit).reduce(_ & _)
     val bIs2x2 = bs.map(_.isUnit).reduce(_ & _)
     if (aIs2x2 && bIs2x2) {
@@ -168,13 +175,15 @@ class Matrix[T <: Data:RealBits](val elB: CustomBundle[CustomBundle[T]], val dep
     }
   }
   def * (b: Matrix[T]): Matrix[T] = {
-    val as = this.split()
-    val bs = b.split()
+    val as = this.split
+    val bs = b.split
     val aIs2x2 = as.map(_.isUnit).reduce(_ & _)
     val bIs2x2 = bs.map(_.isUnit).reduce(_ & _)
     if (aIs2x2 && bIs2x2) {
       val Seq(a11, a12, a21, a22) = as.map(_.el.flatten.head)
       val Seq(b11, b12, b21, b22) = bs.map(_.el.flatten.head)
+    /*
+      // Strassen Winograd
       val s1 = a21 context_+ a22
       val s2 = s1 context_- a11
       val s3 = a11 context_- a21
@@ -196,14 +205,49 @@ class Matrix[T <: Data:RealBits](val elB: CustomBundle[CustomBundle[T]], val dep
       val c12 = t1 context_+ m5 context_+ m6
       val c21 = t2 context_- m7
       val c22 = t2 context_+ m5
+    */
+      // Strassen
+      val m1a = a11 context_+ a22
+      val m1b = b11 context_+ b22
+      val m2a = a21 context_+ a22
+      val m2b = b11
+      val m3a = a11
+      val m3b = b12 context_- b22
+      val m4a = a22
+      val m4b = b21 context_- b11
+      val m5a = a11 context_+ a12
+      val m5b = b22
+      val m6a = a21 context_- a11
+      val m6b = b11 context_+ b12
+      val m7a = a12 context_- a22
+      val m7b = b21 context_+ b22
+      val Seq(m1, m2, m3, m4, m5, m6, m7) = DspContext.withNumMulPipes(1) {
+        val m1 = m1a context_* m1b
+        val m2 = m2a context_* m2b
+        val m3 = m3a context_* m3b
+        val m4 = m4a context_* m4b
+        val m5 = m5a context_* m5b
+        val m6 = m6a context_* m6b
+        val m7 = m7a context_* m7b
+        Seq(m1, m2, m3, m4, m5, m6, m7)
+      }
+      val c11a = m1 context_+ m4
+      val c11b = m7 context_- m5
+      val c22a = m1 context_- m2
+      val c22b = m3 context_+ m6
+      val c12 = m3 context_+ m5
+      val c21 = m2 context_+ m4
+      val c11 = c11a context_+ c11b
+      val c22 = c22a context_+ c22b
       val out = Seq(Seq(c11, c12), Seq(c21, c22))
-      val result = Matrix(out, depth + 1)
+      val result = ShiftRegister(Matrix(out, depth + 1), 1)
       result.suggestName(s"mul_depth${result.depth}")
       result
     }
     else {
       val Seq(a11, a12, a21, a22) = as
       val Seq(b11, b12, b21, b22) = bs
+    /*
       val s1 = a21 + a22
       val s2 = s1 - a11
       val s3 = a11 - a21
@@ -225,7 +269,37 @@ class Matrix[T <: Data:RealBits](val elB: CustomBundle[CustomBundle[T]], val dep
       val c12 = t1 + m5 + m6
       val c21 = t2 - m7
       val c22 = t2 + m5
-      putTogether(c11, c12, c21, c22, "mul")
+    */
+      val m1a = a11 + a22
+      val m1b = b11 + b22
+      val m2a = a21 + a22
+      val m2b = b11
+      val m3a = a11
+      val m3b = b12 - b22
+      val m4a = a22
+      val m4b = b21 - b11
+      val m5a = a11 + a12
+      val m5b = b22
+      val m6a = a21 - a11
+      val m6b = b11 + b12
+      val m7a = a12 - a22
+      val m7b = b21 + b22
+      val m1 = ShiftRegister(m1a * m1b, 1)
+      val m2 = ShiftRegister(m2a * m2b, 1)
+      val m3 = ShiftRegister(m3a * m3b, 1)
+      val m4 = ShiftRegister(m4a * m4b, 1)
+      val m5 = ShiftRegister(m5a * m5b, 1)
+      val m6 = ShiftRegister(m6a * m6b, 1)
+      val m7 = ShiftRegister(m7a * m7b, 1)
+      val c11a = m1 + m4
+      val c11b = m7 - m5
+      val c22a = m1 - m2
+      val c22b = m3 + m6
+      val c12 = m3 + m5
+      val c21 = m2 + m4
+      val c11 = c11a + c11b
+      val c22 = c22a + c22b
+      ShiftRegister(putTogether(c11, c12, c21, c22, "mul"), 1)
     }
   }
 }
@@ -277,29 +351,69 @@ class MatrixOp[T <: Data:RealBits](
 }
 
 class MatrixOpTester[T <: Data:RealBits](testMod: TestModule[MatrixOp[T]]) extends DspTester(testMod) {
+
+  def rotateLeft[A](seq: Seq[A], i: Int): Seq[A] = {
+    val size = seq.size
+    val (first, last) = seq.splitAt(i % size)
+    last ++ first
+  }
+
   val tDut = testMod.dut
+  val n = tDut.n
   val isLit = tDut.op.startsWith("lit")
   val in = tDut.litSeq
-  val n = tDut.n
-  in.zipWithIndex foreach { case (value, idx) => 
-    poke(testMod.getIO("a").asInstanceOf[CustomBundle[T]](idx), value)
-    if (!isLit) poke(testMod.getIO("b").asInstanceOf[CustomBundle[T]](idx), value)
+  val tvs = in.indices.map(x => rotateLeft(in, x))
+  val litMatrix = DenseVector(tvs(0).toArray).toDenseMatrix.reshape(n, n)
+
+  var pipelineDepth = 0
+
+  for (i <- 0 until 2 * in.length) {
+    tvs(i % tvs.length).zipWithIndex foreach { case (value, idx) =>
+      poke(testMod.getIO("a").asInstanceOf[CustomBundle[T]](idx), value)
+      if (!isLit) poke(testMod.getIO("b").asInstanceOf[CustomBundle[T]](idx), value)
+    }
+    if (pipelineDepth == 0) {
+      val breezeMatrix = litMatrix
+      val expected = tDut.op match {
+        case "add" => breezeMatrix + breezeMatrix
+        case "sub" => breezeMatrix - breezeMatrix
+        case "mul" => breezeMatrix * breezeMatrix
+        case "litAdd" => breezeMatrix + litMatrix
+        case "litSub" => breezeMatrix - litMatrix
+        case "litMul" => breezeMatrix * litMatrix
+      }
+      val firstCorrect = expected.toDenseVector.toArray.toSeq.zipWithIndex.map { case (value, idx) =>
+        peek(testMod.getIO("out").asInstanceOf[CustomBundle[T]](idx)) == value
+      }.reduce(_ & _)
+      if (firstCorrect) pipelineDepth = i
+    }
+    else {
+      val breezeMatrix = DenseVector(tvs((i - pipelineDepth) % tvs.length).toArray).toDenseMatrix.reshape(n, n)
+      val expected = tDut.op match {
+        case "add" => breezeMatrix + breezeMatrix
+        case "sub" => breezeMatrix - breezeMatrix
+        case "mul" => breezeMatrix * breezeMatrix
+        case "litAdd" => breezeMatrix + litMatrix
+        case "litSub" => breezeMatrix - litMatrix
+        case "litMul" => breezeMatrix * litMatrix
+      }
+      expected.toDenseVector.toArray.toSeq.zipWithIndex foreach { case (value, idx) =>
+        expect(testMod.getIO("out").asInstanceOf[CustomBundle[T]](idx), value)
+      }
+    }
+    step(1)
   }
-  val breezeMatrix = DenseVector(in.toArray).toDenseMatrix.reshape(n, n)
-  val expected = tDut.op match {
-    case "litAdd" | "add" => breezeMatrix + breezeMatrix
-    case "litSub" | "sub" => breezeMatrix - breezeMatrix
-    case "litMul" | "mul" => breezeMatrix * breezeMatrix
-  }
-  expected.toDenseVector.toArray.toSeq.zipWithIndex foreach { case (value, idx) =>
-    expect(testMod.getIO("out").asInstanceOf[CustomBundle[T]](idx), value)
-  }
+  println(s"Pipeline depth: $pipelineDepth")
+  require(pipelineDepth > 0, "Pipeline depth shouldn't be 0!")
 }
 
 class MatrixOpSpec extends FlatSpec with Matchers {
+
   val n = 8
+
   val len = n * n
   val in = (0 until len).map(_.toDouble)
+
   val inI = Interval(range"[0, ${len}).0")
   val outI = Interval(range"[?, ?].0")
   val inF = FixedPoint((BigInt(len - 1).bitLength + 1).W, 0.BP)
@@ -325,8 +439,7 @@ class MatrixOpSpec extends FlatSpec with Matchers {
       c => new MatrixOpTester(c)
     } should be (true)
   }
-*/  
-
+*/
   it should "properly multiply - FixedPoint" in {
     dsptools.Driver.execute(() => new TestModule(() => new MatrixOp(inF, outF, n, "mul", in)), IATest.options("MatrixMul-F")) {
       c => new MatrixOpTester(c)
@@ -359,5 +472,7 @@ class MatrixOpSpec extends FlatSpec with Matchers {
 */    
 }
 
-// TODO: Pipeline matrix multiply, Why is DspReal just not connected at the input? (Does Complex work?)
+// TODO: Why is DspReal just not connected at the input? (Does Complex work?)
 // n =4, n = 8 have compilation problems
+// chiselTesters, dspTools
+// systolic array matrix mul
