0d74a41ecd5b30ad3cdf2dfcb09cae4583441c7f
Angie <shunshou@gmail.com>
2017-11-13 04:53:37 +0900

    working fir example

--- src/test/scala/toys/FIR.scala
+++ src/test/scala/toys/FIR.scala
@@ -4,45 +4,46 @@ import breeze.math.Complex
 import chisel3._
 import dsptools.intervals.tests.IATest
 import dsptools.numbers._
-import breeze.linalg.{DenseVector, DenseMatrix}
-import generatortools.io.CustomBundle
 import dsptools.{NoTrim, DspContext, DspTester}
+import chisel3.internal.firrtl.IntervalRange
 
 import chisel3.experimental._
 import generatortools.testing.TestModule
-import chisel3.internal.firrtl.{UnknownWidth, KnownWidth, IntervalRange, KnownBinaryPoint}
 
 import org.scalatest.{Matchers, FlatSpec}
-
-import scala.util.Random
 
 class FilterIO[T <: Data:Ring:ConvertableTo](genI: => T, genO: => T) extends Bundle {
   val in = Input(genI)
   val out = Output(genO)
-  val tap0 = Output(genI)
-  val tap1 = Output(genI)
 }
 
-// y[n] = sum b_i * x[n - i]
+// y[n] = sum { b_i * x[n - i] }
 @chiselName
-class FIRFilter[T <: Data:Ring:ConvertableTo](genI: => T, genO: => T, bp: Int, mp: Int, ap: Int, val coeffs: Seq[Double]) extends Module {
+class FIR[T <: Data:Ring:ConvertableTo](genI: => T, genO: => T, bp: Int, pipes: Int, val coeffs: Seq[Double]) extends Module {
   val io = IO(new FilterIO(genI, genO))
-  DspContext.alter(DspContext.current.copy(trimType = NoTrim, numMulPipes = mp, numAddPipes = ap, binaryPoint = Some(bp))) {
-    val taps = (1 until coeffs.length).scanLeft(io.in)((in, _) => RegNext(in, init = ConvertableTo[T].fromDouble(0.0)))
-    io.tap0 := taps(0)
-    io.tap1 := taps(1)
-    val coeffsT = coeffs.map(c => ConvertableTo[T].fromDouble(c))
-    io.out := taps.zip(coeffsT) map { case (t, c) => t context_* c } reduce(_ context_+ _)
+  DspContext.alter(DspContext.current.copy(trimType = NoTrim, numMulPipes = pipes, binaryPoint = Some(bp))) {
+    val taps = coeffs.tail.scanLeft(io.in)((in, _) => RegNext(in, init = ConvertableTo[T].fromDouble(0.0)))
+    val coeffsT = coeffs map (c => ConvertableTo[T].fromDouble(c))
+    io.out := taps zip (coeffsT) map { case (t, c) => t context_* c } reduce (_ context_+ _)
   }
 }
 
-class FIRFilterTester[T <: Data:Ring:ConvertableTo](testMod: TestModule[FIRFilter[T]], tvs: Seq[Double]) extends DspTester(testMod) {
+
+
+/*
+class FIR[T <: Data:Ring:ConvertableTo](genI: => T, genO: => T, bp: Int, mp: Int, ap: Int, val coeffs: Seq[Double]) extends Module {
+  val io = IO(new FilterIO(genI, genO))
+  DspContext.alter(DspContext.current.copy(trimType = NoTrim, numMulPipes = mp, numAddPipes = ap, binaryPoint = Some(bp))) {
+    val taps = coeffs.tail.scanLeft(io.in)((in, _) => RegNext(in, init = ConvertableTo[T].fromDouble(0.0)))
+    val coeffsT = coeffs map (c => ConvertableTo[T].fromDouble(c))
+    io.out := taps zip (coeffsT) map { case (t, c) => t context_* c } reduce (_ context_+ _)
+  }
+}
+*/
+
+class FIRFilterTester[T <: Data:Ring:ConvertableTo](testMod: TestModule[FIR[T]], tvs: Seq[Double]) extends DspTester(testMod) {
   val tDut = testMod.dut
   val coeffs = tDut.coeffs
-
-  print(coeffs)
-  println("BLAH")
-  println(tvs.mkString(", "))
 
   tDut.io.in match {
     case i: Interval =>
@@ -50,60 +51,79 @@ class FIRFilterTester[T <: Data:Ring:ConvertableTo](testMod: TestModule[FIRFilte
     case c: DspComplex[_] =>
       poke(testMod.getIO("in").asInstanceOf[DspComplex[_]], Complex(0.0, 0.0))
   }
+
   reset(5)
-  //peek(testMod.getIO("tap0"))
-  //peek(testMod.getIO("tap1"))
   step(1)
-  for (idx <- 0 until tvs.length) {
+
+  for (idx <- tvs.indices) {
     tDut.io.in match {
       case i: Interval =>
         poke(testMod.getIO("in"), tvs(idx))
       case c: DspComplex[_] =>
         poke(testMod.getIO("in").asInstanceOf[DspComplex[_]], Complex(tvs(idx), -tvs(idx)))
     }
-    //peek(testMod.getIO("tap0"))
-    //peek(testMod.getIO("tap1"))
+    // Accounts for 1 cycle (hardcoded) multiplication latency
     if (idx >= coeffs.length) {
-      // Accounts for 1 cycle multiplication latency
+      // Earliest is the right-most in the chain
       val currTaps = tvs.slice(idx - coeffs.length, idx).reverse
-      println(s"$currTaps")
       tDut.io.out match {
         case i: Interval =>
-          val exp = currTaps.zip(coeffs) map { case (t, c) =>
-            println(s"t: $t c: $c")
-            t * c
-          } reduce(_ + _)
+          val exp = currTaps.zip(coeffs) map { case (t, c) => t * c } reduce (_ + _)
           expect(testMod.io("out"), exp)
         case c: DspComplex[_] =>
-          val exp = currTaps.zip(coeffs) map { case (tap, coeff) => Complex(tap, -tap) * coeff } reduce(_ + _)
+          val exp = currTaps.zip(coeffs) map { case (t, c) => Complex(t, -t) * c } reduce (_ + _)
           expect(testMod.io("out").asInstanceOf[DspComplex[_]], exp)
       }
     }
     step(1)
   }
 }
-
+/*
 class FIRFilterSpec extends FlatSpec with Matchers {
   val inI = Interval(range"[-16, 16).2")
   val outI = Interval(range"[?, ?].4")
-  val coeffs = inI.range.getPossibleValues.take(2)
+  val coeffs = inI.range.getPossibleValues.take(64)
   // TODO: Separate function!
-  val randomTVs = MatMulTests.generateRandomInputs(math.sqrt(coeffs.length).toInt, 10, maxNotInclusive = 15).flatten
+  val randomTVs = MatMulTests.generateRandomInputs(math.sqrt(coeffs.length).toInt, 20, maxNotInclusive = 15).flatten
 
   behavior of "FIR Filter"
 
   it should "properly filter -- Interval" in {
     val name = s"FIRFilterI"
-    dsptools.Driver.execute(() => new TestModule(() => new FIRFilter(inI, outI, bp = 2, mp = 1, ap = 0, coeffs), name = name), IATest.options(name, backend = "firrtl", fixTol = 1, verbose = true)) {
-        c => new FIRFilterTester(c, randomTVs)
+    dsptools.Driver.execute(() => new TestModule(() => new FIR(inI, outI, bp = 2, mp = 1, ap = 0, coeffs), name = name), IATest.options(name, backend = "firrtl", fixTol = 1)) {
+        c => new FIRTester(c, randomTVs)
     } should be(true)
   }
 
   it should "properly filter -- Complex" in {
+    val name = s"FIRFilterC"
+    dsptools.Driver.execute(() => new TestModule(() => new FIR(DspComplex(inI), DspComplex(outI), bp = 2, mp = 1, ap = 0, coeffs), name = name), IATest.options(name, backend = "firrtl", fixTol = 1)) {
+      c => new FIRTester(c,randomTVs)
+    } should be(true)
+  }
+}
+*/
+
+class FIRFilterSpec extends FlatSpec with Matchers {
+  val inI = Interval(range"[-16, 16).2")
+  val outI = Interval(range"[?, ?].4")
+  val coeffs = inI.range.getPossibleValues.take(64)
+  // TODO: Separate function!
+  val randomTVs = MatMulTests.generateRandomInputs(math.sqrt(coeffs.length).toInt, 20, maxNotInclusive = 15).flatten
+
+  behavior of "FIR Filter"
+
+  it should "properly filter -- Interval" in {
     val name = s"FIRFilterI"
-    dsptools.Driver.execute(() => new TestModule(() => new FIRFilter(DspComplex(inI), DspComplex(outI), bp = 2, mp = 1, ap = 0, coeffs), name = name), IATest.options(name, backend = "firrtl", fixTol = 1, verbose = true)) {
-      c => new FIRFilterTester(c,randomTVs)
+    dsptools.Driver.execute(() => new TestModule(() => new FIR(Interval(range"[-16, 16).2"), Interval(range"[?, ?].4"), bp = 2, pipes = 1, coeffs), name = name), IATest.options(name, backend = "firrtl", fixTol = 1)) {
+      c => new FIRFilterTester(c, randomTVs)
     } should be(true)
   }
 
+  it should "properly filter -- Complex" in {
+    val name = s"FIRFilterC"
+    dsptools.Driver.execute(() => new TestModule(() => new FIR(DspComplex(Interval(range"[-16, 16).2")), DspComplex(Interval(range"[?, ?].4")), bp = 2, pipes = 1, coeffs), name = name), IATest.options(name, backend = "firrtl", fixTol = 1)) {
+      c => new FIRFilterTester(c,randomTVs)
+    } should be(true)
+  }
 }
