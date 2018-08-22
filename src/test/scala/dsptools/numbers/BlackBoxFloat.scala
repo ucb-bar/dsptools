9210732296fb52b397303fea1a45e227005934a2
chick <chick@qrhino.com>
2018-08-22 11:39:07 -0700

    Add treadle hooks for DspReals Treadle uses a slightly different BlackBox scala implementation scheme This PR adds the required factory and new black box methods. - All black boxes need getOutput method - Separate Treadle version of factory added. - Add this factory to the treadle options in Driver - Removed extra resources that caused duplicate code erros with verilator in BlackBoxFloatSpec

--- src/test/scala/dsptools/numbers/BlackBoxFloat.scala
+++ src/test/scala/dsptools/numbers/BlackBoxFloat.scala
@@ -251,12 +251,7 @@ class FloatOpTester[T <: FloatOps](c: T, testTrigFuncs: Boolean = true) extends 
 
 class BlackBoxFloatSpec extends ChiselFlatSpec {
   "A BlackBoxed FP block" should "work" in {
-    assertTesterPasses({ new BlackBoxFloatTester },
-        Seq(
-          "/BBFAdd.v",
-          "/BBFMultiply.v",
-          "/BBFEquals.v"
-          ))
+    assertTesterPasses({ new BlackBoxFloatTester })
   }
 
   "basic addition" should "work with reals through black boxes" in {
